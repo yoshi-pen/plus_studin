aaef8e525b50c3a62c348bf3b80a51b333269a74
Masaki Yoshikawa <j2370rgg@sit.ac.jp>
2025-12-05 14:19:27 +0900

    Add project files

---
+++ test.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Masaki Yoshikawa
+# SPDX-Licence-Indentifier: BSD-3-Clause
+#
+ng(){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+#out=$(echo あ | ./plus)
+#[ "$?" = 1 ]      || ng ${LINENO}
+#[ "${out}" = "" ] || ng ${LINENO}
+
+#out=$(echo | ./plus)
+#[ "$?" = 1 ]      || ng ${LINENO}
+#[ "${out}" = "" ] || ng ${LINENO}
+
+
+[ "${res}" = 0 ] && echo OK
+exit $res
